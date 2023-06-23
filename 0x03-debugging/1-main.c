eba8b5a83c4072a67cf5a533e03969e57bd7dfa1
Mumoh254 <peteritumo2030@gmail.com>
2023-06-23 10:17:19 -0700

    0 > 972?

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,5 @@
+Copy this main file. Comment out (don't delete it!) the part of the code that is causing the output to go into an infiniteloop.
+
+Don't add ot remove any lines of code,as we will be checking your line count. You are only allowed to comment out existing code.
+
+You do not have to compile with-Wall-Werror-Wextra-pedantic fot this task.
