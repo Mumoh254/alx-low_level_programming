d370ac949fe6a44d8400d53c382136ddd07b0d9d
Mumoh254 <peteritumo2030@gmail.com>
2023-06-24 23:10:21 -0700

    like share comment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,3 +5,33 @@ Don’t add or remove any lines of code, as we will be checking your line count.
 
 You do not have to compile with -Wall -Werror -Wextra -pedantic for this task.
 
+
+
+
+
+
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}
+
