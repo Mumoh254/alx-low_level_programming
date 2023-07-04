d30d75be0c64bb94c622c2ee7e99883990db0f3c
Mumoh254 <peteritumo2030@gmail.com>
2023-07-03 20:42:09 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,38 @@
-Prototype: char *_strncat(char *dest, char *src, int n);
-The _strncat function is similar to the _strcat function, except that
-it will use at most n bytes from src; and
-src does not need to be null-terminated if it contains n or more bytes
-Return a pointer to the resulting string dest
-FYI: The standard library provides a similar function: strncat. Run man strncat to learn more.
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+
+
+
+
+
+
 
